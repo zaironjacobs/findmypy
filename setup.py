d68b65c6ceca39d374f18e73dc959050b137b953
Zairon Jacobs <zaijacobs@gmail.com>
2022-09-21 06:26:37 +0200

    refactor

--- setup.py
+++ setup.py
@@ -1,41 +1,40 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-REPO_URL = "https://github.com/LoPablo/findmypy"
-VERSION = "0.1.11"
-
+REPO_URL = 'https://github.com/zaironjacobs/findmypy'
+VERSION = '0.1.11'
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / 'README.md').read_text()
 
 setup(
-    name="findmypy",
-    author="LoPablo",
-    author_email="lopablo@protonmail.com",
+    name='findmypy',
+    author='LoPablo',
+    author_email='lopablo@protonmail.com',
     description="Library to use Apple's FindMy Api.",
     long_description=long_description,
     long_description_content_type='text/markdown',
     version=VERSION,
     url=REPO_URL,
-    packages=find_packages(include=["findmypy"]),
+    packages=find_packages(include=['findmypy']),
     package_data={
-        "": ["AppleCA.pem"],
+        '': ['AppleCA.pem'],
     },
-    python_requires=">=3.7",
-    license="MIT",
+    python_requires='>=3.7',
+    license='MIT',
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Software Development :: Libraries",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries',
     ],
-    keywords=["icloud", "find-my-iphone"],
+    keywords=['icloud', 'find-my-iphone'],
 )
